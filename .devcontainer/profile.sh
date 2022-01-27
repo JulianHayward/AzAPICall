05102b0a81e6f330a57ca806f9911603b3ef740c
quoteee <45695032+JulianHayward@users.noreply.github.com>
2022-01-27 08:50:58 +0000

    codespaces opt

--- .devcontainer/profile.sh
+++ .devcontainer/profile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env pwsh
 $profilePath = Split-Path -Path $PROFILE
 New-Item -ItemType Directory -Force -Path $profilePath
-$scriptFolder = 'pwsh'
-echo "cd $scriptFolder" >> $PROFILE
+#$scriptFolder = 'pwsh'
+#echo "cd $scriptFolder" >> $PROFILE
