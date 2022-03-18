7fa16b629b11d7cdebe186423bec6d20c97d3585
Kai Schulz <kai.a.schulz@basf.com>
2022-03-18 09:31:24 +0100

    Optimize build process

--- pwsh/module/buildModule.ps1
+++ pwsh/module/buildModule.ps1
@@ -6,7 +6,27 @@ if (Test-Path .\pwsh\module\build\AzAPICall\functions\AzAPICallFunctions.ps1) {
         Remove-Item -Path .\pwsh\module\build\AzAPICall\functions\AzAPICallFunctions.ps1 -Verbose -ErrorAction Stop
     }
     catch {
-        Write-Host ' Cleaning build/functions failed'
+        Write-Host ' Cleaning build/functions for AzAPICallFunctions.ps1 failed'
+        throw
+    }
+}
+
+if (Test-Path .\pwsh\module\build\AzAPICall\AzAPICall.psd1) {
+    try {
+        Remove-Item -Path .\pwsh\module\build\AzAPICall\AzAPICall.psd1 -Verbose -ErrorAction Stop
+    }
+    catch {
+        Write-Host ' Cleaning build for AzAPICall.psd1 failed'
+        throw
+    }
+}
+
+if (Test-Path .\pwsh\module\build\AzAPICall\AzAPICall.psm1) {
+    try {
+        Remove-Item -Path .\pwsh\module\build\AzAPICall\AzAPICall.psm1 -Verbose -ErrorAction Stop
+    }
+    catch {
+        Write-Host ' Cleaning build for AzAPICall.psm1 failed'
         throw
     }
 }
@@ -16,4 +36,21 @@ Get-ChildItem -path .\pwsh\module\dev\AzAPICall\functions | ForEach-Object -Proc
     $fileContent = Get-Content -Path .\pwsh\module\dev\AzAPICall\functions\$($PSItem.Name) -Raw
     $fileContent | Add-Content -Path .\pwsh\module\build\AzAPICall\functions\AzAPICallFunctions.ps1
 }
+
+try {
+    Copy-Item -Path .\pwsh\module\dev\AzAPICall\AzAPICall.psd1 -Destination .\pwsh\module\build\AzAPICall -Verbose -ErrorAction Stop
+}
+catch {
+    Write-Host ' Copy AzAPICall.psd1 not possible'
+    Throw
+}
+
+try {
+    Copy-Item -Path .\pwsh\module\dev\AzAPICall\AzAPICall.psm1 -Destination .\pwsh\module\build\AzAPICall -Verbos -ErrorAction Stop
+}
+catch {
+    Write-Host ' Copy AzAPICall.psm1 not possible'
+    Throw
+}
+
 Write-Host 'Building one file containing all functions done'
